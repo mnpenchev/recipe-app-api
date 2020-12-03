5ba14fa33678e2c8de7a8fc96c8be6ccd0ee7dcf
mnpenchev <mnpenchev@gmail.com>
2020-12-03 16:24:29 +0000

    added manage user endpoint

--- app/user/urls.py
+++ app/user/urls.py
@@ -7,4 +7,5 @@ app_name = 'user'
 urlpatterns = [
     path('create/', CreateUserView.as_view(), name='create'),
     path('token', CreateTokenView.as_view(), name='token'),
+    path('me/', ManageUserView.as_view(), name='me'),
 ]
