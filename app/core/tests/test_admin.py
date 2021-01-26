7dc489b2190569a71eb746d72aa73b7760a1a58a
mnpenchev <53950572+mnpenchev@users.noreply.github.com>
2021-01-26 19:01:28 +0000

    Update test_admin.py

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -7,12 +7,12 @@ class AdminSiteTest(TestCase):
     def setUp(self):
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
-            email='admin@londonappdev.com',
+            email='admin@admin.com',
             password='password123'
         )
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
-            email='test@londonappdev.com',
+            email='test@admin.com',
             password='password123',
             name='Test user full name'
         )
